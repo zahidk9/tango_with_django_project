5707c3eb795bf94ad38974f333f2d2039bbc7426
Zahid Khan <2404574k@student.gla.ac.uk>
2021-01-29 12:47:42 +0000

    End of Ch4 w/ error

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
+
+def about(request):
+    context_dict = {'boldmessage': 'This tutorial has been put together by Zahid Khan'}
+    return render(request, 'rango/about.html', context=context_dict)
 
 
