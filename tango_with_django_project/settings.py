39e74a6c187d7bba3f7d032285538eca24ff05b6
Zahid Khan <2404574k@student.gla.ac.uk>
2021-02-12 18:40:32 +0000

    End of Ch10

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
  
 
 
@@ -133,3 +134,7 @@ STATIC_URL = '/static/'
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+
+#Login URL
+LOGIN_URL = 'rango:login'
